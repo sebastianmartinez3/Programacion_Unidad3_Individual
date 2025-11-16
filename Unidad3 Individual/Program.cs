57190cb796cc5b6495cdc6ee33aa272ff9f111af
Sebastian Martinez Perez <smartinez@pitasoft.com>
2025-11-16 20:37:08 +0100

    Add initial Unidad3 Individual project
    - Created `global.json` to specify .NET SDK version and configuration.
    - Added core `Program.cs` skeleton with menu and placeholder methods.
    - Added .csproj file targeting .NET 8.0 framework.
    - Updated `.gitignore` to include Visual Studio and .NET specific entries.
    - Generated solution file for Visual Studio.

---
+++ Unidad3 Individual/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Unidad3_Individual;
+
+class Program
+{
+    // Pista
+    // static [Clase] _usuario
+    // static [Clase] _libro
+    
+    static void Main(string[] args)
+    {
+        string? entrada;
+        Console.WriteLine("Actividad Unidad 3 (Individual)");
+        Console.WriteLine("Creada por: ");
+        Menu();
+        do
+        {
+            entrada = Console.ReadLine();
+            switch (entrada)
+            {
+                case "0":
+                    Menu();
+                    break;
+                case "1":
+                    RegistrarUsuario();
+                    break;
+                case "2":
+                    ConsultarUsuario();
+                    break;
+                case "3":
+                    RegistrarUsuario();
+                    break;
+                case "4":
+                    ConsultarLibro();
+                    break;
+                case "9":
+                    break;
+                default:
+                    Console.WriteLine("Opcion no valida");
+                    break;
+            }
+
+        } while (entrada!= "9");
+        Console.WriteLine("Fin de apicación");
+    }
+
+    static void Menu()
+    {
+        Console.WriteLine("=== Menu principal ===");
+        Console.WriteLine("0. Menu principal");
+        Console.WriteLine("1. Registrar usuario");
+        Console.WriteLine("2. Consultar usuario");
+        Console.WriteLine("3. Registrar libro");
+        Console.WriteLine("4. Consultar libro");
+        Console.WriteLine("----------------------");
+        Console.WriteLine("9. Salir");
+    }
+
+    static void RegistrarUsuario()
+    {
+        
+    }
+
+    static void ConsultarUsuario()
+    {
+        
+    }
+
+    static void RegistrarLibro()
+    {
+        
+    }
+
+    static void ConsultarLibro()
+    {
+        
+    }
+}
